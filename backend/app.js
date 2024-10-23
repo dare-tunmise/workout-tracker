d155916a418bc32fed4b775e8c48735765c53615
dare-tunmise <daretunmise@yahoo.com>
2024-10-23 08:47:15 +0100

    ...

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,23 @@
 require('dotenv').config();
 const express = require('express');
-const PORT = process.env.PORT
-const app = express();
 const workoutRoutes = require('./routes/workouts');
+const PORT = process.env.PORT
 
+//invoke express
+const app = express();
+
+//set up middleware
+
+app.use(express.json())
 app.use((req, res, next)=>{
     (console.log(req.path, req.method));
     next();
 })
 
+//routes
 app.use('/api/workouts', workoutRoutes);
 
+//listen for requests
 app.listen(PORT, ()=>{
     console.log(`listening on port ${PORT}`)
 });
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -9,11 +9,24 @@ router.get('/', (req, res)=>{
 })
 
 //GET a single workouts
-
 router.get('/:id', (req, res)=> {
-    const id = req.params.id;
-    
+    // const id = req.params.id;
+    res.json({mssg: 'GET a single workout'}) 
 })
 
+//POST  a new workout
+router.post('/', (req, res)=> {
+    res.json({mssg: "POST a new workout"})
+})
+
+//DELETE  a new workout
+router.delete('/:id', (req, res)=> {
+    res.json({mssg: "DELETE a workout"})
+})
+
+//UPDATE  a new workout
+router.patch('/:id', (req, res)=> {
+    res.json({mssg: "UPDATE a workout"})
+})
 
 module.exports = router;
