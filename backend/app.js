b15d580dee9061e924d5ac703aa099f595ae30a1
dare-tunmise <daretunmise@yahoo.com>
2024-10-23 07:26:23 +0100

    first commit

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+app.listen(3000, ()=>{
+    console.log('listening on port 3000')
+});
+
+app.get('/', (req, res)=> {
+    res.json({msg: "update guys"})
+})
