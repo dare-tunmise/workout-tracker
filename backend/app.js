a1c2a4f0dbc67f26b6cc2a420d66599715e56270
dare-tunmise <daretunmise@yahoo.com>
2024-10-23 09:15:28 +0100

    created a file for schema & models

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,22 @@
 require('dotenv').config();
 const express = require('express');
 const workoutRoutes = require('./routes/workouts');
+const { default: mongoose } = require('mongoose');
 const PORT = process.env.PORT
-
+const dbURI = process.env.dbURI
 //invoke express
 const app = express();
+
+//connect to the database
+mongoose.connect(dbURI)
+    .then((result)=> {
+        app.listen(PORT, ()=>{
+            console.log(`listening on port ${PORT}`)
+        });
+    })
+    .catch((err)=> {
+        console.log(err);
+    })
 
 //set up middleware
 
@@ -16,8 +28,3 @@ app.use((req, res, next)=>{
 
 //routes
 app.use('/api/workouts', workoutRoutes);
-
-//listen for requests
-app.listen(PORT, ()=>{
-    console.log(`listening on port ${PORT}`)
-});
