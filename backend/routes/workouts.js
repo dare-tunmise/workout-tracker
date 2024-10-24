8f5937ee52614c49d70bc9179f052ea93ac37fb6
dare-tunmise <daretunmise@yahoo.com>
2024-10-24 22:53:33 +0100

    creating controllers

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Workout = require('../models/WorkoutModel')
+
 
 const router = express.Router();
 
@@ -17,14 +17,7 @@ router.get('/:id', (req, res)=> {
 
 //POST  a new workout
 router.post('/', async (req, res)=> {
-    const {title, load, reps } = req.body
 
-    try {
-        const workout = await Workout.create({title, loads, reps})
-        res.status(200).json(workout)
-    } catch (error) {
-        res.status(400).json({error: error.message} );
-    }
 
 })
 
