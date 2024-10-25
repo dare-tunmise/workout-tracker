838dee3aa20f475158df6221fe1db73f69600f42
dare-tunmise <daretunmise@yahoo.com>
2024-10-25 23:16:41 +0100

    ...

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -3,6 +3,8 @@ const {
     getWorkouts,
     getWorkout,
     createWorkout,
+    deleteWorkout,
+    updateWorkout
 } = require('../controllers/workoutController')
 
 
@@ -19,13 +21,9 @@ router.get('/:id', getWorkout)
 router.post('/', createWorkout)
 
 //DELETE  a new workout
-router.delete('/:id', (req, res)=> {
-    res.json({mssg: "DELETE a workout"})
-})
+router.delete('/:id', deleteWorkout)
 
 //UPDATE  a new workout
-router.patch('/:id', (req, res)=> {
-    res.json({mssg: "UPDATE a workout"})
-})
+router.patch('/:id', updateWorkout)
 
 module.exports = router;
