3c174f943776d710498ea1449be24fbb90d7c423
dare-tunmise <daretunmise@yahoo.com>
2024-10-28 23:23:32 +0100

    ...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-  
+      <BrowserRouter>
+        <div className='pages'>
+          <Routes>
+            <Route
+             path='/'
+             element={}
+            />
+          </Routes>
+        </div>
+      
+      </BrowserRouter>
     </div>
   );
 }
